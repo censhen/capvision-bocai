f8b1fd76c25c93b2def3837949500ba21150ef4d
cshen <cshen@capvision.com>
2015-06-16 14:08:55 +0800

    add bocai module

---
+++ modules/bocai/Module.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace app\modules\bocai;
+
+class Module extends \yii\base\Module
+{
+    public $controllerNamespace = 'app\modules\bocai\controllers';
+
+    public function init()
+    {
+        parent::init();
+
+        // custom initialization code goes here
+    }
+}
